696201573fc7d8ad6884503b810027d67db03988
Gabriel M. Specian <ga.specian@gmail.com>
2024-11-22 11:01:50 -0300

    feat: criar metodo Save para realizar update e insert de produtos

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -27,3 +27,57 @@ func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
 	}
 	return &product, nil
 }
+
+func (p *ProductDb) Save(productInterface application.ProductInterface) (application.ProductInterface, error) {
+	var rows int
+	p.db.QueryRow("select id from products where id=?", productInterface.GetId()).Scan(&rows)
+	if rows == 0 {
+		_, err := p.create(productInterface)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		_, err := p.update(productInterface)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return productInterface, nil
+}
+
+func (p *ProductDb) create(productInterface application.ProductInterface) (application.ProductInterface, error) {
+	stmt, err := p.db.Prepare(`insert into products(id, name, price, status) values (?, ?, ?, ?)`)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = stmt.Exec(
+		productInterface.GetId(),
+		productInterface.GetName(),
+		productInterface.GetPrice(),
+		productInterface.GetStatus(),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = stmt.Close()
+	if err != nil {
+		return nil, err
+	}
+	return productInterface, nil
+}
+
+func (p *ProductDb) update(productInterface application.ProductInterface) (application.ProductInterface, error) {
+	_, err := p.db.Exec("update products set name = ?, price= ?, status= ? where id= ?",
+		productInterface.GetName(),
+		productInterface.GetPrice(),
+		productInterface.GetStatus(),
+		productInterface.GetId(),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return productInterface, nil
+}
